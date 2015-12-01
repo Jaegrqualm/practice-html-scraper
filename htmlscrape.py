da485b34947aba22ea4c3eca758490f186c06a75
Roger <rh964@live.missouristate.edu>
2015-11-30 20:18:36 -0600

    final for project

--- htmlscrape.py
+++ htmlscrape.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests
+import sys
 
 #get main page
 main_page = requests.get('http://distrowatch.com')
@@ -7,9 +8,46 @@ tree = html.fromstring(main_page.content)
 
 #read top 100 into lists
 ranks = tree.xpath('//th[@class="phr1"]/text()')
-names = tree.xpath('//th[@class="phr2"]//a[@href=/text()')
-scores = tree.xpath('//th[@class="phr3" @title="Yesterday: "/text()]')
+names = tree.xpath('//td[@class="phr2"]//a/@href')
+scores = tree.xpath('//td[@class="phr3"]/text()')
 
 print(ranks)
 print(names)
 print(scores)
+
+distros_info = []
+count = 0
+#get the data for each distro
+for x in names:
+	URL = 'http://distrowatch.com/table.php?distribution=' + x
+	distro_page = requests.get(URL)
+	tree2 = html.fromstring(distro_page.content)
+	distro_data = tree2.xpath('//tr/td[1]/text()')
+	#remove useless data
+	distro_info = []
+	for y in range(len(distro_data) -  61, len(distro_data),1):
+		distro_info.append(distro_data[y])
+	
+	distros_info.append(distro_info) 
+		
+	#get the per-distro data right	
+	output = ''
+	for y in range(len(distros_info[count])):
+		output = output + distros_info[count][y] + ','
+	
+	#write it all to a file
+	with open('/home/roger/Documents/python/distrowatchdata.csv','a') as f:		
+		f.write(ranks[count] + ',' + names[count] + ',' + scores[count] + ',' + output + '\n')
+
+	count+=1
+	print('done: ' + str(count))
+
+	
+
+		
+
+	
+
+		
+
+		
---
+++ test.py
@@ -0,0 +1,35 @@
+from lxml import html
+import requests
+
+
+distro_page = requests.get('http://distrowatch.com/table.php?distribution=lxle')
+tree = html.fromstring(distro_page.content)
+
+distro_data = tree.xpath('//tr/td[1]/text()')
+#print(distro_data[148])
+print( len(distro_data))
+
+#distro_data = tree.xpath('//tr/td[1]/text()')
+
+distro_info = []
+for y in range(len(distro_data) -  61, len(distro_data) - 8,1):
+	distro_info.append(distro_data[y])
+
+print(distro_info)
+
+
+
+
+'''
+#remove useless data
+for y in range(156,148):
+#int y = 156, y >= 148, y--:
+	distro_data.pop(y)
+	y+=1
+
+for y in range(95,0):
+	distro_data.pop(y)
+	y+=1
+		
+print(distro_data)
+'''
